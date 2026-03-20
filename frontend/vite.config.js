56042a2406cebc4ec02e5f0a4f12a2943f5e7400
Alan_Gutierrez <ala.gutierrezv@duocuc.cl>
2026-03-20 19:01:46 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js'
-  }
+    globals: true,
+    setupFiles: './src/test/setup.js',
+  },
 })
